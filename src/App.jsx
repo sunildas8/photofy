be9e4f71c937246bea5691974d5614e5d26b381f
Sunil Das <95643764+sunildas1@users.noreply.github.com>
2025-02-16 19:05:53 +0600

    complete navbar section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+        <Route path='/result' element={<Result/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
