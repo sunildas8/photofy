1ba9fbd7655dad4eb560c49c5d37a9e991119791
Sunil Das <95643764+sunildas1@users.noreply.github.com>
2025-02-22 19:15:06 +0600

    complete a sing-up and login section

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,8 +5,9 @@ import { AppContext } from '../context/Appcontext'
 
 const Navbar = () => {
 
-    const {user} = useContext(AppContext)
+  const { user, setShowLogin } = useContext(AppContext);
     const nativgate = useNavigate()
+
 
   return (
     <div className='flex items-center justify-between py-4'>
@@ -36,7 +37,7 @@ const Navbar = () => {
             : 
              <div className='flex items-center gap-2 sm:gap-5'> 
                 <p onClick={()=>nativgate('/buy')} className='text-primary cursor-pointer'>Pricing</p>
-                <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+                <button onClick={()=>setShowLogin(true)} className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full cursor-pointer'>Login</button>
              </div>
             }
         </div>
--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -4,16 +4,13 @@ export const AppContext = createContext()
 
 const AppContextProvider = ({ children })=>{
     const [user,setUser] = useState(null)
+    const [showLogin,setShowLogin] = useState(false)
     
     return (
-        <AppContext.Provider value={{ user, setUser }}>
+        <AppContext.Provider value={{ user, setUser, showLogin, setShowLogin }}>
           {children}
         </AppContext.Provider>
     );
 }
 
-
 export default AppContextProvider
-
-
-
